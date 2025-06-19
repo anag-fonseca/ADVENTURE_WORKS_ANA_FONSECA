7fa5830db88073cb2681ec24e8474208e9ee6cc0
Ana Fonseca <ana.fonseca@indicium.tech>
2025-06-19 22:14:03 +0000

    finalizacao dimensoes

--- models/intermediate/int_cliente__enriched.sql
+++ models/intermediate/int_cliente__enriched.sql
@@ -2,10 +2,10 @@ with
 -- import models
 person as (
     select 
-        person_id,
-        person_tipo,
-        person_email,
-        person_demografia,
+        person_id as cliente_id,
+        person_tipo as tipo_cliente,
+        person_email as email,
+        person_demografia as demografia,
         concat_ws(' ', person_primeiro_nome, person_nome_meio, person_ultimo_nome) as nome_completo
     from {{ ref('stg_pessoa__persons')}}
 )
--- models/intermediate/int_vendas__enriched.sql
+++ models/intermediate/int_vendas__enriched.sql
@@ -1,6 +1,6 @@
 with 
 -- import models
-salesorderdatail as (
+salesorderdetail as (
     select *
     from {{ ref('stg_vendas__salesorderdetail')}}
 )
@@ -11,12 +11,26 @@ salesorderdatail as (
 
 -- transformaçao para construção dos dados de venda
 
---,--joined as (
---    select 
---
---, from stg_pagamento__creditcard
---    left join personcreditcard on stg_pagamento__creditcard.creditcard_id = stg_pagamento__personcreditcard.personcreditcard_cartaoid
-----)
---
---select *
---from joined
+, joined as (
+    select 
+        salesorderheader.salesorderheader_pedido_id as pedido_id,
+        salesorderheader.salesorderheader_data_pedido as data_pedido,
+        salesorderheader.salesorderheader_status as status,
+        salesorderheader.salesorderheader_cliente_id as cliente_id,
+        salesorderheader.salesorderheader_vendedor_id as vendedor_id,
+        salesorderheader.salesorderheader_territorio_id as territorio_id,
+        salesorderheader.salesorderheader_cartao_id as cartao_id,
+        salesorderheader.salesorderheader_subtotal as subtotal,
+        salesorderheader.salesorderheader_imposto as imposto,
+        salesorderheader.salesorderheader_frete as frete,
+        salesorderheader.salesorderheader_totaldue as total,
+        salesorderdetail.salesorderdetail_quantidade_pedido as quantidade_pedido,
+        salesorderdetail.salesorderdetail_produto_id as produto_id,
+        salesorderdetail.salesorderdetail_preco_unitario as preco_unitario,
+        salesorderdetail.salesorderdetail_preco_unitario_desconto as preco_unitario_desconto,
+from salesorderheader 
+    left join salesorderdetail on salesorderheader.salesorderheader_pedido_id = salesorderdetail.salesorderdetail_pedido_id
+)
+
+select *
+from joined
